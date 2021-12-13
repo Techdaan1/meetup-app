f6d889300fd969b0eab951adc63aa84efde3f7b6
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-12-13 14:28:51 +0100

    Update App.test.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,10 +2,16 @@ import React from "react";
 import { shallow } from "enzyme";
 import App from "../App";
 import EventList from "../EventList";
+import CitySearch from "../CitySearch";
 
 describe("<App /> component", () => {
   test("render list of events", () => {
     const AppWrapper = shallow(<App />);
     expect(AppWrapper.find(EventList)).toHaveLength(1);
+  });
+
+  test("render CitySearch", () => {
+    const AppWrapper = shallow(<App />);
+    expect(AppWrapper.find(CitySearch)).toHaveLength(1);
   });
 });
