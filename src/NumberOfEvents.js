59ffdef56995d58417471d5fc70d8ac4f214a144
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-12-14 20:14:41 +0100

    Build

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class NumberOfEvents extends Component {
   render() {
-    return <div></div>;
+    return <div className="NumberOfEvents"></div>;
   }
 }
 
