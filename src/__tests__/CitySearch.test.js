aacc0d96a2326a81f75570a06616cc926d0bfc76
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-12-14 09:06:58 +0100

    Update CitySearch.test.js

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -7,4 +7,15 @@ describe("<CitySearch /> component", () => {
     const CitySearchWrapper = shallow(<CitySearch />);
     expect(CitySearchWrapper.find(".city")).toHaveLength(1);
   });
+
+  test("renders a list of suggestions", () => {
+    const CitySearchWrapper = shallow(<CitySearch />);
+    expect(CitySearchWrapper.find(".suggestions")).toHaveLength(1);
+  });
+
+  test("render text input correctly", () => {
+    const CitySearchWrapper = shallow(<CitySearch />);
+    const query = CitySearchWrapper.state("query");
+    expect(CitySearchWrapper.find(".city").prop(value)).toBe(query);
+  });
 });
