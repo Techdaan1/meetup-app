1e0444564e449870b6368f9190b68728d0396944
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-12-14 09:07:03 +0100

    Update CitySearch.js

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 
 class CitySearch extends Component {
+  state = {
+    query: "",
+  };
   render() {
     return (
       <div className="CitySearch">
-        <input type="text" className="city" />
+        <input type="text" className="city" value={this.state.query} />
+        <ul className="suggestions"></ul>
       </div>
     );
   }
