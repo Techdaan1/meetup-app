610d50baab05f887c003d0d61418d916e234906d
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-12-14 11:43:53 +0100

    Update CitySearch.js

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -8,7 +8,16 @@ class CitySearch extends Component {
 
   handleInputChanged = (event) => {
     const value = event.target.value;
-    this.setState({ query: value });
+    const suggestions = this.props.locations.filter((location) => {
+      return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+    });
+    this.setState({ query: value, suggestions });
+  };
+
+  handleItemClicked = (suggestion) => {
+    this.setState({
+      query: suggestion,
+    });
   };
 
   render() {
@@ -22,9 +31,14 @@ class CitySearch extends Component {
         />
         <ul className="suggestions">
           {this.state.suggestions.map((suggestion) => (
-            <li key={suggestion}>{suggestion}</li>
+            <li
+              key={suggestion}
+              onClick={() => this.handleItemClicked(suggestion)}
+            >
+              {suggestion}
+            </li>
           ))}
-          <li key="all">
+          <li>
             <b>See all cities</b>
           </li>
         </ul>
