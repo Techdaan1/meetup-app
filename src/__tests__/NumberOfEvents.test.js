8dc34f524ba169598866ba4d79de9929070473b0
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-12-16 09:39:22 +0100

    Update NumberOfEvents.test.js

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { shallow } from "enzyme";
 import NumberOfEvents from "../NumberOfEvents";
-import { mockData } from "../mock-data";
 
 describe("<NumberOfEvents /> component", () => {
   let NumberOfEventsWrapper;
   beforeAll(() => {
     NumberOfEventsWrapper = shallow(<NumberOfEvents />);
   });
+
+  test("render textbox input", () => {
+    expect(NumberOfEventsWrapper.find(".newValue")).toHaveLength(1);
+  });
+
+  test("Change event on textbox", () => {
+    expect(NumberOfEventsWrapper);
+  });
 });
-
-test("render textbox input");
-
-test("Change event on textbox");
