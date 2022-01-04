2e941181c2f59ed1c0b34d65c549403f558ca31b
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2022-01-04 11:58:32 +0100

    import getEvents from ./api

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -5,7 +5,7 @@ import EventList from "../EventList";
 import CitySearch from "../CitySearch";
 import NumberOfEvents from "../NumberOfEvents";
 import { mockData } from "../mock-data";
-import { extractLocations } from "../api";
+import { extractLocations, getEvents } from "../api";
 
 describe("<App /> component", () => {
   let AppWrapper;
