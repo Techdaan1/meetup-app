f0c88642cbe44f9d50735ce2b6ee004fc7dcad33
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2022-01-04 12:03:48 +0100

    Update App.test.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -56,7 +56,7 @@ describe("<App /> integration", () => {
     await CitySearchWrapper.instance().handleItemClicked(selectedCity);
     const allEvents = await getEvents();
     const eventsToShow = allEvents.filter(
-      (event) => event.locations === selectedCity
+      (event) => event.location === selectedCity
     );
     expect(AppWrapper.state("events")).toEqual(eventsToShow);
     AppWrapper.unmount();
