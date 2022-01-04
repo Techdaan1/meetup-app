740e2a5bea77858935a22527e0c1f83f2076284d
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2022-01-04 11:59:33 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,32 @@ import React, { Component } from "react";
 import "./App.css";
 import EventList from "./EventList";
 import CitySearch from "./CitySearch";
+import { getEvents } from "./api";
 
 class App extends Component {
   state = {
     events: [],
     locations: [],
   };
+
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents = events.filter(
+        (event) => event.location === location
+      );
+      this.setState({
+        events: locationEvents,
+      });
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <CitySearch locations={this.state.locations} />
+        <CitySearch
+          locations={this.state.locations}
+          updateEvents={this.updateEvents}
+        />
         <EventList events={this.state.events} />
       </div>
     );
