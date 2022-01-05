c98e34e891a1476ccc4b18bc46ecddf86648cec9
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2022-01-05 11:11:38 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
 import { mockData } from "./mock-data";
+import axios from "axios";
 
 export const extractLocations = (events) => {
   var extractLocations = events.map((event) => event.location);
@@ -6,6 +7,33 @@ export const extractLocations = (events) => {
   return locations;
 };
 
+const checkToken = async (accessToken) => {
+  const result = await fetch(
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+  )
+    .then((res) => res.json())
+    .catch((error) => error.json());
+
+  return result;
+};
+
 export const getEvents = async () => {
   return mockData;
 };
+
+export const getAccessToken = async () => {
+  const accessToken = localStorage.getItem("access_token");
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.locations.search);
+    const code = await searchParams.get(window.location.search);
+    if (!code)
+    const results = await axios.get("https://mlzydonigl.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"),
+    const {authUrl} = results.data;
+    return (window.location.href = authUrl);
+  }
+  return code && getToken(code);
+}
+return accessToken;
