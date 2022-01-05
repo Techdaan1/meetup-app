03ee827e7e6f8ec3cbb3a9b6c3fc5df09f3c4e30
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2022-01-05 12:25:23 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -70,17 +70,22 @@ export const getEvents = async () => {
   }
 };
 
-if (!accessToken || tokenCheck.error) {
-  await localStorage.removeItem("access_token");
-  const searchParams = new URLSearchParams(window.location.search);
-  const code = await searchParams.get("code");
-  if (!code) {
-    const results = await axios.get(
-      "https://mlzydonigl.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
-    );
-    const { authUrl } = results.data;
-    return (window.location.href = authUrl);
+export const getAccessToken = async () => {
+  const accessToken = localStorage.getItem("access_token");
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    if (!code) {
+      const results = await axios.get(
+        "https://mlzydonigl.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
+      );
+      const { authUrl } = results.data;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
   }
-  return code && getToken(code);
-}
-return accessToken;
+  return accessToken;
+};
