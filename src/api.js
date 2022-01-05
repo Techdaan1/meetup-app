ff0f4b8617c75f6a000cd183e03dd7a38eede20c
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2022-01-05 12:01:43 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,5 +1,7 @@
 import { mockData } from "./mock-data";
 import axios from "axios";
+import NProgress from "nprogress";
+
 
 export const extractLocations = (events) => {
   var extractLocations = events.map((event) => event.location);
@@ -17,8 +19,53 @@ const checkToken = async (accessToken) => {
   return result;
 };
 
+const removeQuery = () => {
+  if (window.history.pushState && window.location.pathname) {
+    var newurl =
+    window.location.protocol + 
+    "//" +
+    window.location.pathname;
+    window.history.pushState("", "", newurl);
+  } else {
+    newurl = window.location.protocol + "//" + window.location.host;
+    window.history.pushState("", "", newurl);
+  }
+}
+
+const getToken = async (code) => {
+  const encodeCode = encodeURIComponent(code);
+  const { access_token } = await fetch("https://mlzydonigl.execute-api.eu-central-1.amazonaws.com/dev/api/token" + "/" + encodeCode)
+  .then((res) => {
+    return res.json();
+  })
+  .catch((error) => error);
+  access_token && localStorage.setItem("access_token", access_token);
+
+  return access_token;
+}
+
 export const getEvents = async () => {
-  return mockData;
+  NProgress.start();
+
+  if (window.location.href.startsWith("http://localhost")) {
+    NProgress.done();
+    return mockData;
+  }    
+  const token = await getAccessToken();
+
+  if (token) {
+    removeQuery();
+    const url = "https://mlzydonigl.execute-api.eu-central-1.amazonaws.com/dev/api/get-events" + "/" + token;
+    const result = await axios.get(url);
+    if (result.data) {
+      var locations = extractlocations(result.data.events);
+      localStorage.setItem("lastEvents", JSON.stringify(results.data));
+      localStorage.setItem("locations", JSON.stringify(locations));
+      NProgress.done();
+      return result.data.events;
+    }
+  }
+
 };
 
 export const getAccessToken = async () => {
