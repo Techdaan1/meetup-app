822d843c82e691cee5ab48bf4a80a2f71fb7d28d
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2022-01-11 10:38:35 +0100

    Update App.test.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -23,7 +23,7 @@ describe("<App /> component", () => {
   });
 
   test("render number of events", () => {
-    expect(AppWrapper.find(NumberOfEvents)).toHaveLength(0);
+    expect(AppWrapper.find(NumberOfEvents)).toHaveLength(1);
   });
 });
 
