f2d2304a504620bfbdab62c7e72bce0601ca5dc4
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2022-01-17 12:12:56 +0100

    update

--- src/index.js
+++ src/index.js
@@ -16,14 +16,15 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+//serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-//testing error event
+// testing error event
 atatus.config("f312b1402b28473aa55be8174476bfab").install();
 
 //atatus.notify(new Error("Test Atatus Setup"));
