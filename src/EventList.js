ce9cd50733a12da4b4f3eb65a97968087bc7221e
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2022-01-18 11:11:13 +0100

    Added WarningAlert

--- src/EventList.js
+++ src/EventList.js
@@ -1,17 +1,26 @@
 import React, { Component } from "react";
 import Event from "./Event";
+import { Container } from "react-bootstrap";
+import { WarningAlert } from "./Alert";
 
 class EventList extends Component {
   render() {
     const { events } = this.props;
     return (
-      <ul className="eventlist">
-        {events.map((event) => (
-          <li key={event.id}>
-            <Event event={event} />
-          </li>
-        ))}
-      </ul>
+      <Container className="eventlist-container">
+        {!navigator.online ? (
+          <WarningAlert text="You are offline! The displayed event list has been loaded from the cache." />
+        ) : (
+          <WarningAlert text="" />
+        )}
+        <ul className="eventlist">
+          {events.map((event) => (
+            <li key={event.id}>
+              <Event event={event} />
+            </li>
+          ))}
+        </ul>
+      </Container>
     );
   }
 }
