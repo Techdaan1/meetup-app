3497d4246c682388318612a630e618a046e9e85e
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2022-01-18 13:37:24 +0100

    Update WelcomeScreen.jsx

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./WelcomeScreen.css";
+
 function WelcomeScreen(props) {
   return props.showWelcomeScreen ? (
     <div className="WelcomeScreen">
@@ -8,10 +9,10 @@ function WelcomeScreen(props) {
         Log in to see upcoming events around the world for full-stack developers
       </h4>
       <div className="button_cont" align="center">
-        <div class="google-btn">
-          <div class="google-icon-wrapper">
+        <div className="google-btn">
+          <div className="google-icon-wrapper">
             <img
-              class="google-icon"
+              className="google-icon"
               src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Log
 o.svg"
               alt="Google sign-in"
