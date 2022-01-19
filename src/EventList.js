abaac1a35625c97b76a0d24f6f32a0ce8e28d184
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2022-01-19 10:10:47 +0100

    Update EventList.js

--- src/EventList.js
+++ src/EventList.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Event from "./Event";
-import { Container } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import { WarningAlert } from "./Alert";
 
 class EventList extends Component {
@@ -8,18 +8,15 @@ class EventList extends Component {
     const { events } = this.props;
     return (
       <Container className="eventlist-container">
-        {!navigator.online ? (
-          <WarningAlert text="You are offline! The displayed event list has been loaded from the cache." />
-        ) : (
-          <WarningAlert text="" />
-        )}
-        <ul className="eventlist">
-          {events.map((event) => (
-            <li key={event.id}>
-              <Event event={event} />
-            </li>
-          ))}
-        </ul>
+        <Row>
+          <ul className="eventlist">
+            {events.map((event) => (
+              <li key={event.id}>
+                <Event event={event} />
+              </li>
+            ))}
+          </ul>
+        </Row>
       </Container>
     );
   }
