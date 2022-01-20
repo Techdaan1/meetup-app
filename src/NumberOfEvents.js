b96e90c568fcf6578469082233a3bf6dd155df8b
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2022-01-20 14:01:40 +0100

    Update NumberOfEvents.js

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -9,9 +9,10 @@ class NumberOfEvents extends Component {
   };
 
   handleInputChanged = (event) => {
-    const number = event.target.value;
-    if (number <= 0 || number > 32) {
+    const number = event.target.number;
+    if (number < 1 || number > 32) {
       this.setState({
+        numberOfEvents: "",
         infoText: "Enter number between 1 and 32",
       });
     } else {
@@ -20,6 +21,7 @@ class NumberOfEvents extends Component {
         infoText: "",
       });
     }
+    this.props.updateNumberOfEvents(event.target.number);
   };
 
   render() {
@@ -31,8 +33,8 @@ class NumberOfEvents extends Component {
             <input
               type="number"
               className="new-number-of-events"
-              value={this.state.numberOfEvents}
-              onChange={this.handleInputChanged}
+              value={this.props.numberOfEvents}
+              onChange={(e) => this.handleInputChanged}
             />
           </Col>
           <Col>
