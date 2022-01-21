9474ccd4a0a385c777ffdf03139cee913d46b41a
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2022-01-21 09:07:25 +0100

    Create EventGenre.js

---
+++ src/EventGenre.js
@@ -0,0 +1,6 @@
+import React, { PureComponent } from "react";
+import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
+
+const EventGenre = ({ events }) => {};
+
+export default EventGenre;
