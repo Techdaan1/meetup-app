772bf749a9534971eab3251ba2622125af1d1906
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2022-01-21 09:37:14 +0100

    Update EventGenre.js

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -1,6 +1,32 @@
-import React, { PureComponent } from "react";
+import React, { useEffect, useState } from "react";
 import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
 
-const EventGenre = ({ events }) => {};
+const EventGenre = ({ events }) => {
+  const data = [
+    { name: "Group A", value: 400 },
+    { name: "Group B", value: 300 },
+    { name: "Group C", value: 300 },
+    { name: "Group D", value: 200 },
+  ];
+
+  return (
+    <ResponsiveContainer height={400}>
+      <PieChart width={400} height={400}>
+        <Pie
+          data={data}
+          cx={200}
+          cy={200}
+          labelLine={false}
+          outerRadius={80}
+          fill="#8884d8"
+          dataKey="value"
+          label={({ name, percent }) =>
+            `${name} ${(percent * 100).toFixed(0)}%`
+          }
+        ></Pie>
+      </PieChart>
+    </ResponsiveContainer>
+  );
+};
 
 export default EventGenre;
