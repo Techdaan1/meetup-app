752256b04b32366555a46f65e97470fb48d9285c
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2022-01-21 11:10:55 +0100

    Update EventGenre.js

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -2,24 +2,21 @@ import React, { useEffect, useState } from "react";
 import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
 
 const EventGenre = ({ events }) => {
-  const data = [
-    { name: "Group A", value: 400 },
-    { name: "Group B", value: 300 },
-    { name: "Group C", value: 300 },
-    { name: "Group D", value: 200 },
-  ];
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const getData = () => {
+      const genres = ["React", "JavaScript", "Node", "jQuery", "AngularJS"];
 
-  const getData = () => {
-    const genres = ["React", "JavaScript", "Node", "jQuery", "AngularJS"];
-
-    const data = genres.map((genre) => {
-      const value = events.filter(({ summary }) =>
-        summary.split(" ").includes(genre)
-      ).length;
-      return { name: genre, value };
-    });
-    return data;
-  };
+      const data = genres.map((genre) => {
+        const value = events.filter(({ summary }) =>
+          summary.split(" ").includes(genre)
+        ).length;
+        return { name: genre, value };
+      });
+      return data;
+    };
+    setData(() => getData());
+  }, [events]);
 
   return (
     <ResponsiveContainer height={400}>
